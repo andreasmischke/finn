eb3227e88e86e9b1a5e62f5feb429a9a27033611
Andreas Mischke <andreas@mischke.me>
2017-01-29 21:26:37 +0100

    Added placeholders to main menu

--- app/scenes/MainMenuScene.js
+++ app/scenes/MainMenuScene.js
@@ -6,12 +6,24 @@ export default class MainMenuScene extends Scene {
 
     render(scene) {
         scene.classList.add('main_menu');
+        scene.appendChild(this.create_button_placeholder('Eingangsszene'));
         scene.appendChild(this.create_menu_button('Bei Emil', 'emil'));
-        scene.appendChild(this.create_menu_button('Bei Emma', 'emma'));
         scene.appendChild(this.create_menu_button('TreeHouse Game', 'treehouse_game'));
+        scene.appendChild(this.create_menu_button('Bei Emma', 'emma'));
         scene.appendChild(this.create_menu_button('Shop Game', 'shop_game'));
+        scene.appendChild(this.create_button_placeholder('Bei Papa'));
         scene.appendChild(this.create_menu_button('Office Game', 'office_game'));
+        scene.appendChild(this.create_button_placeholder('Bei Karl'));
         scene.appendChild(this.create_menu_button('Egg Game', 'egg_game'));
+        scene.appendChild(this.create_button_placeholder('In der Scheune'));
+
+    }
+
+    create_button_placeholder(text) {
+        return create_element('div')
+            .class('button_placeholder')
+            .text(text)
+            .render();
     }
 
     create_menu_button(text, target) {
