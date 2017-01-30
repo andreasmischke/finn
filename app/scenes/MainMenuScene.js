0cdee1c36f29b9061ea4e6e2a5ed1e89599c3c7d
Andreas Mischke <andreas@mischke.me>
2017-01-30 21:16:28 +0100

    Finished MainMenu optically

--- app/scenes/MainMenuScene.js
+++ app/scenes/MainMenuScene.js
@@ -18,42 +18,84 @@ export default class MainMenuScene extends Scene {
         this.scene = scene;
         scene.classList.add('main_menu');
 
-        this.create_story_button();
+        this.create_story_buttons();
+        this.create_menu_buttons();
+
+        /*
         this.create_menu_button('Eingangsszene', PrefaceScene);
         this.create_menu_button('Bei Emil - Teil 1', EmilScene, 'in');
-        this.create_menu_button('Baumhaus-Spiel', TreehouseGameScene);
         this.create_menu_button('Bei Emil - Teil 2', EmilScene, 'out');
         this.create_menu_button('Bei Emma - Teil 1', EmmaScene, 'in');
-        this.create_menu_button('Wiege-Spiel', ShopGameScene);
         this.create_menu_button('Bei Emma - Teil 2', EmmaScene, 'out');
         this.create_menu_button('Bei Papa - Teil 1', PapaScene, 'in');
-        this.create_menu_button('Ordner-Spiel', OfficeGameScene);
         this.create_menu_button('Bei Papa - Teil 2', PapaScene, 'out');
         this.create_menu_button('Bei Karl - Teil 1', KarlScene, 'in');
-        this.create_menu_button('Eier-Spiel', EggGameScene);
         this.create_menu_button('Bei Karl - Teil 2', KarlScene, 'mid');
         this.create_button_placeholder('In der Scheune');
         this.create_menu_button('Bei Karl - Teil 3', KarlScene, 'out');
+        */
     }
 
-    create_story_button() {
-        this.scene.appendChild(create_element('div')
-            .class('menu_button')
+    create_story_buttons() {
+        const section = create_element('div')
+            .class('section');
+
+        section.adopt(create_element('div')
+            .class('heading')
+            .class('heading_story')
             .click(e => story.play())
-            .text("Story")
+            .text("Geschichte spielen")
             .render());
+
+        section.adopt(create_element('div')
+            .class('btn')
+            .class('btn_play_story')
+            .click(e => story.play())
+            .text("Von vorne starten")
+            .render());
+
+        section.adopt(create_element('div')
+            .class('btn')
+            .class('btn_resume_story')
+            .click(e => story.play())
+            .text("Weiterspielen")
+            .render());
+
+        this.scene.appendChild(section.render());
+    }
+
+    create_menu_buttons() {
+        const section = create_element('div')
+            .class('section');
+
+        this.game_section = section.render();
+
+        section.adopt(create_element('div')
+            .class('heading')
+            .class('heading_games')
+            .click(e => story.play())
+            .text("Einzelspiele spielen")
+            .render());
+
+        this.create_menu_button('Nägel zählen', TreehouseGameScene);
+        this.create_menu_button('Artikel wiegen', ShopGameScene);
+        this.create_menu_button('Ordner sortieren', OfficeGameScene);
+        this.create_menu_button('Eier einpacken', EggGameScene);
+
+        this.scene.appendChild(section.render());
     }
 
     create_button_placeholder(text) {
         this.scene.appendChild(create_element('div')
+            .class('btn')
             .class('button_placeholder')
             .text(text)
             .render());
     }
 
     create_menu_button(text, target, ...args) {
-        this.scene.appendChild(create_element('div')
-            .class('menu_button')
+        this.game_section.appendChild(create_element('div')
+            .class('btn')
             .click(e => scene_manager.navigate(new target(...args)))
             .text(text)
             .render());
